219bd196b585b4f425f0d48e6ec197595057a47b
Muhammad Zuhairi <47267077+mzuhairiy@users.noreply.github.com>
2026-04-09 20:52:59 +0700

    fix

--- utils/config.ts
+++ utils/config.ts
@@ -13,8 +13,8 @@ interface Config {
 const config: Config = {
     baseURL: process.env.BASE_URL || 'https://staging.petpals-demo.shop',
     validUser: {
-        email: process.env.TEST_USER_EMAIL || '',
-        password: process.env.TEST_USER_PASSWORD || '',
+        email: process.env.TEST_USER_EMAIL || 'garaga@petpals.com',
+        password: process.env.TEST_USER_PASSWORD || '@admin123',∏
     },
 };
 
